2b486ad61b193f54d95940bbdda8f0b9e81454c5
Aquila Sands <5774614+AquilaSands@users.noreply.github.com>
2020-02-03 10:13:00 +0000

    Minimal repro

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using AADSqlAuthRepro.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AADSqlAuthRepro.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index(DatabaseDetails database)
+        {
+            if (!string.IsNullOrWhiteSpace(database.Server))
+            {
+                // Connect to Azure SQL with AAD password auth
+
+                SqlConnectionStringBuilder csb = GetConnectionStringBuilder(database);
+
+                try
+                {                    
+                    database.SuccessfulConnection = await IsAzureSqlAsync(csb.ConnectionString);
+                }
+                catch (Exception e)
+                {
+                    database.ErrorMessage = e.Message;
+                    Exception inner = e.InnerException;
+
+                    while (!(inner is null))
+                    {
+                        database.ErrorMessage = inner.Message;
+                        inner = inner.InnerException;
+                    };
+                }
+            }
+            
+            return View(database);
+        }
+
+        private static SqlConnectionStringBuilder GetConnectionStringBuilder(DatabaseDetails database) => new SqlConnectionStringBuilder
+        {
+            DataSource = database.Port == 0 ? database.Server : $"{database.Server},{database.Port}",
+            InitialCatalog = database.DatabaseName,
+            UserID = database.UserName,
+            Password = database.Password,
+            Authentication = SqlAuthenticationMethod.ActiveDirectoryPassword,
+            IntegratedSecurity = false,
+            Encrypt = true,
+            TrustServerCertificate = false,
+            ConnectRetryCount = 5,
+            ConnectTimeout = 30
+        };
+
+        private async Task<bool> IsAzureSqlAsync(string connectionString)
+        {
+            using var conn = new SqlConnection(connectionString);
+            using var cmd = new SqlCommand
+            {
+                Connection = conn,
+                CommandType = CommandType.Text,
+                CommandText = "SELECT SERVERPROPERTY('Edition')"
+            };
+
+            await conn.OpenAsync();
+
+            using SqlDataReader reader = await cmd.ExecuteReaderAsync();
+            await reader.ReadAsync();
+
+            return reader.GetString(0).Equals("SQL Azure", StringComparison.OrdinalIgnoreCase);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/DatabaseDetails.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AADSqlAuthRepro.Models
+{
+    public class DatabaseDetails 
+    {
+        [HiddenInput]
+        public bool SuccessfulConnection { get; set; }
+        
+        [Required]
+        [Display(Name = "Server Address")]
+        public string Server { get; set; }
+
+        [Display(Name = "Port")]
+        public int Port { get; set; } = 1433;
+
+        [Required]
+        [Display(Name = "Database Name")]
+        public string DatabaseName { get; set; }
+
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [HiddenInput]   
+        public string ErrorMessage { get; set; }
+    }
+}
